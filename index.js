934d8ea052d9b8cf6b78bf1d2f1baf4f7f7c0b17
egapradana260-jpg <egapradana260@gmail.com>
2026-09-20 01:19:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
+const mineflayer = require('mineflayer');
+
+function createBot() {
+  const bot = mineflayer.createBot({
+    host: 'Scapleois.aternos.me', // GANTI dengan IP server Aternos kamu
+    port: 17760,                         // GANTI dengan Port server Aternos kamu
+    username: 'STAFF SCAPLEOSIS',            // Nama bot di Minecraft
+    version: false
+  });
+
+  bot.on('spawn', () => {
+    console.log('Bot berhasil masuk ke server!');
+    
+    // Gerakan acak dan melompat tiap 15 detik biar gak kena kick AFK
+    setInterval(() => {
+      bot.setControlState('jump', true);
+      setTimeout(() => bot.setControlState('jump', false), 500);
+
+      const controls = ['forward', 'back', 'left', 'right'];
+      const randomControl = controls[Math.floor(Math.random() * controls.length)];
+      
+      bot.setControlState(randomControl, true);
+      setTimeout(() => bot.setControlState(randomControl, false), 1000);
+    }, 15000);
+  });
+
+  bot.on('end', () => {
+    console.log('Bot terputus, mencoba masuk lagi dalam 30 detik...');
+    setTimeout(createBot, 30000);
+  });
+
+  bot.on('error', err => console.log('Error:', err));
+}
+
+createBot();
 
